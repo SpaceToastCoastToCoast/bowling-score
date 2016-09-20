19c84fb5e85f01c294cec159cc11980c5e59d6b4
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-20 09:30:44 -1000

    wrote first test: the score calculator should return a number

--- test/bowling.spec.js
+++ test/bowling.spec.js
@@ -3,5 +3,9 @@ const bowling = require('../bowling.js');
 const expect = chai.expect;
 
 describe('bowling score calculator', function() {
-
+  it('should be a function that returns a number representing the total score', function() {
+    expect(bowling).to.exist;
+    expect(bowling).to.be.a('function');
+    expect(bowling()).to.be.a('number');
+  });
 });
