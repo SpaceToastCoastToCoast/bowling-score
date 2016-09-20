12e2061171bc2a93c8f3c661a3a53ec5543bf38f
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-20 09:46:07 -1000

    passed second test: calculates score over 10 frames of play

--- bowling.js
+++ bowling.js
@@ -1,4 +1,13 @@
-module.exports = function() {
+module.exports = function(frames) {
   var score = 0;
+
+  for(var frame in frames) {
+    var frameTotal = 0;
+    for(var f in frames[frame]) {
+      frameTotal += frames[frame][f];
+    }
+    score += frameTotal;
+  }
+
   return score;
 };
