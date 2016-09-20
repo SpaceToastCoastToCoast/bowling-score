2e9256e054462ccc3554243ec99d491ce6a4ca3f
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-20 09:53:26 -1000

    passed spare bonus test

--- bowling.js
+++ bowling.js
@@ -1,10 +1,18 @@
 module.exports = function(frames) {
   var score = 0;
+  var spareBonus = false;
 
   for(var frame in frames) {
     var frameTotal = 0;
     for(var f in frames[frame]) {
+      if(spareBonus) {
+        frameTotal += frames[frame][0];
+        spareBonus = false;
+      }
       frameTotal += frames[frame][f];
+    }
+    if(frameTotal == 10) {
+      spareBonus = true;
     }
     score += frameTotal;
   }
